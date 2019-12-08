ca5e4143cfe45aea9e22cea36c804a6b68ada9ca
Michał Hajduk <michal.hajduk@e3.pl>
2019-12-08 16:27:13 +0100

    problem with value of thisCartProduct.amountWidge

--- src/js/script.js
+++ src/js/script.js
@@ -374,6 +374,8 @@
       /*add element to menu */
       thisCart.dom.productList.appendChild(thisCart.element);
 
+      thisCart.products.push(new CartProduct(menuProduct, thisCart.element));
+
 
       console.log('adding product', menuProduct);
     }
@@ -383,6 +385,51 @@
 
       thisCart.dom.toggleTrigger.addEventListener('click', function(){
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+    }
+  }
+
+  class CartProduct{
+
+    constructor(menuProduct, element){
+
+      const thisCartProduct = this;
+      thisCartProduct.id = menuProduct.id;
+      thisCartProduct.name = menuProduct.name;
+      thisCartProduct.price = menuProduct.price;
+      thisCartProduct.priceSingle = menuProduct.priceSingle;
+      thisCartProduct.amount = menuProduct.amount;
+
+      thisCartProduct.params = JSON.parse(JSON.stringify(menuProduct.params));
+
+      thisCartProduct.getElements(element);
+      thisCartProduct.initAmountWidget();
+
+      console.log('new cart product',thisCartProduct);
+      console.log('product data', menuProduct);
+    }
+    getElements(element){
+
+      const thisCartProduct = this;
+
+      thisCartProduct.dom = {};
+
+      thisCartProduct.dom.wrapper = element;
+      thisCartProduct.dom.amountWidget = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.amountWidget);
+      thisCartProduct.dom.price = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.price);
+      thisCartProduct.dom.edit = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.edit);
+      thisCartProduct.dom.remove = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.remove);
+    }
+    initAmountWidget() {
+      const thisCartProduct = this;
+      //console.log(thisCartProduct.amountWidget);
+      thisCartProduct.amount = thisCartProduct.dom.amountWidget.value;
+      thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amount;
+      thisCartProduct.amountWidget = new AmountWidget(thisCartProduct.dom.amountWidget);
+      thisCartProduct.amountWidget.addEventListener('updated', function () {
+
+        thisCartProduct.amountWidget.appendChild(thisCartProduct.price);
+
       });
     }
   }
