eb6c123327fc9e80e2b955c405efa75b49ffc1fe
Michał Hajduk <michal.hajduk@e3.pl>
2019-12-19 11:17:02 +0100

    added AJAX problem with undefined url

--- src/js/script.js
+++ src/js/script.js
@@ -51,6 +51,11 @@
       edit: '[href="#edit"]',
       remove: '[href="#remove"]',
     },
+    db: {
+      url: '//localhost:3131',
+      product: 'product',
+      order: 'order',
+    },
     // CODE ADDED END
   };
   
@@ -524,16 +529,35 @@
   const app = {
     initMenu: function () {
       const thisApp = this;
+      
 
       // console.log('thisApp.data:', thisApp.data);
       for (let productData in thisApp.data.products) {
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+
       }
     },
     initData: function () {
       const thisApp = this;
 
-      thisApp.data = dataSource;
+      thisApp.data = {};
+      const url = settings.db.url + '/' + settings.db.product;
+      fetch(url)
+        .then(function(rawResponse){
+          return rawResponse.json();
+        })
+        .then(function(parsedResponse){
+          console.log('parsedResponse', parsedResponse);
+
+          /*save parsedResponse as thisApp.data.products*/ 
+          parsedResponse = thisApp.data.products;
+
+          /*execute initMenu method*/
+          app.initMenu();
+
+        });
+
+      console.log('thisApp.data', JSON.stringify(thisApp.data));
     },
     init: function () {
       const thisApp = this;
